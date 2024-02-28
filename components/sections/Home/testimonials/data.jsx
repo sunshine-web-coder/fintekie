f28c6909bd8d7a9eeef6fc9e50735ae349138f32
sunshine-web-coder <webcoderdev02@gmail.com>
2024-02-28 16:08:41 +0100

    testimonial avatar updated

--- components/sections/Home/testimonials/data.jsx
+++ components/sections/Home/testimonials/data.jsx
@@ -1,25 +1,28 @@
-import avatar from "../../../../assets/avatar1.png";
+import avatar1 from "../../../../assets/avatar1.png";
+import avatar2 from "../../../../assets/avatar2.jpg";
+import avatar3 from "../../../../assets/avatar3.jpg";
 
 export const sliderItems = [
   {
     name: "Tobi Ajayi",
     role: "CEO, Design",
-    avatar: avatar,
+    avatar: avatar1,
     quote:
       "Using this charge card has revolutionized our company's spending habits. The dynamic spending limits, coupled with enticing rewards, have streamlined our financial processes and added significant value to our transactions.",
   },
   {
-    name: "Tobi Ajayi",
-    role: "CEO, Design",
-    avatar: avatar,
+    name: "Ben Nelson",
+    role: "Team Lead",
+    avatar: avatar2,
     quote:
       "Using this charge card has revolutionized our company's spending habits. The dynamic spending limits, coupled with enticing rewards, have streamlined our financial processes and added significant value to our transactions.",
   },
   {
-    name: "Tobi Ajayi",
-    role: "CEO, Design",
-    avatar: avatar,
+    name: "Kate Henshaw",
+    role: "Product Manager",
+    avatar: avatar3,
     quote:
       "Using this charge card has revolutionized our company's spending habits. The dynamic spending limits, coupled with enticing rewards, have streamlined our financial processes and added significant value to our transactions.",
   },
 ];
+
